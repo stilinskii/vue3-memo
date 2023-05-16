1714cad9d6ea0c3bd370cc73220942df8daa56bd
stilinskii <lhy1031hy@gmail.com>
2023-05-17 00:12:26 +0900

    initial

---
+++ src/components/Ground.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ground">
+    <Header />
+    hel
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+export default {
+  components: { Header, Footer },
+};
+</script>
